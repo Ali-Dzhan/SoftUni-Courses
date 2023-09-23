f0ff9fa44036a11f0af3b6bfededfd93dd11016f
Ali-Dzhan Sadak <118597212+Jimikotkata@users.noreply.github.com>
2023-09-23 17:52:17 +0300

    Add files via upload

---
+++ DataTypesAndVariables/Lab/CharsToString.java
@@ -0,0 +1,17 @@
+package DataTypesAndVariables.Lab;
+
+import java.util.Scanner;
+
+public class CharsToString {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String result = "";
+        for (int i = 0; i < 3; i++) {
+            char symbol = scanner.nextLine().charAt(0);
+            result += symbol;
+        }
+        System.out.println(result);
+    }
+}
