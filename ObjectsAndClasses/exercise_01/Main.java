058e3e8229c946375a470beb14a77f7b66443b37
Ali-Dzhan Sadak <118597212+Jimikotkata@users.noreply.github.com>
2024-01-05 15:03:29 +0200

    Add files via upload

---
+++ ObjectsAndClasses/exercise_01/Main.java
@@ -0,0 +1,19 @@
+package ObjectsAndClasses.exercise_01;
+
+import ObjectsAndClasses.exercise_01.Message;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Message message = new Message();
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String output = message.randomMessage();
+            System.out.println(output);
+        }
+    }
+}
