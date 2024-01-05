8c2e4cc278055184fbfe04454ff48cf36ca2f4c0
Ali-Dzhan Sadak <118597212+Jimikotkata@users.noreply.github.com>
2024-01-05 16:01:06 +0200

    Add files via upload

---
+++ Programming_basics_Oct_2022/lab4/NumbersFromNTo1.java
@@ -0,0 +1,15 @@
+package FirstStepsInCoding.Lab.lab4;
+
+import java.util.Scanner;
+
+public class NumbersFromNTo1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for(int i = n; i>=1; i--){
+            System.out.println(i);
+        }
+    }
+}
