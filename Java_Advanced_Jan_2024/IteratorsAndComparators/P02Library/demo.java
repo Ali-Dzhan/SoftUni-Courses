d337d35da4ca2e0d38d202606664038398d2a028
Ali-Dzhan Sadak <118597212+Ali-Dzhan@users.noreply.github.com>
2024-02-10 15:17:52 +0200

    Add files via upload

---
+++ Java_Advanced_Jan_2024/IteratorsAndComparators/P02Library/demo.java
@@ -0,0 +1,17 @@
+package IteratorsAndComparators.P02Library;
+
+public class demo {
+    public static void main(String[] args) {
+      /*int a = 9;
+      int b = 5;
+
+        System.out.println(a%b);*/
+
+
+
+        for (int x=10 ; x>=4 ; x++){
+            System.out.println(x);
+        }
+
+    }
+}
---
+++ Java_Advanced_Jan_2024/IteratorsAndComparators/P03ComparableBook/Main.java
@@ -0,0 +1,24 @@
+/*package IteratorsAndComparators.P03ComparableBook;
+
+public class Main {
+    public static void main(String[] args) {
+        IteratorsAndComparators.P04BookComparator.Book bookOne = new IteratorsAndComparators.P04BookComparator.Book("Animal Farm", 2003, "George Orwell");
+
+        IteratorsAndComparators.P04BookComparator.Book bookTwo = new IteratorsAndComparators.P04BookComparator.Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
+
+        if (bookOne.compareTo(bookTwo) > 0) {
+
+            System.out.printf("%s is before %s%n", bookOne, bookTwo);
+
+        } else if (bookOne.compareTo(bookTwo) < 0) {
+
+            System.out.printf("%s is before %s%n", bookTwo, bookOne);
+
+        } else {
+
+            System.out.println("IteratorsAndComparators.P04BookComparator.Book are equal");
+        }
+    }
+}
+
+ */
