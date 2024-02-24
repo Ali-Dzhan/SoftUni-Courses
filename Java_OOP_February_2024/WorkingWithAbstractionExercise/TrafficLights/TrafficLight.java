e923c1eb93675f6fe74b1db682a834ed954e1d23
Ali-Dzhan Sadak <118597212+Ali-Dzhan@users.noreply.github.com>
2024-02-24 19:43:16 +0200

    Add files via upload

---
+++ Java_OOP_February_2024/WorkingWithAbstractionExercise/TrafficLights/TrafficLight.java
@@ -0,0 +1,29 @@
+package WorkingWithAbstractionExercise.TrafficLights;
+
+public class TrafficLight {
+    private Color color;
+
+    public TrafficLight (Color color) {
+        //нов светофар
+        this.color = color;
+    }
+
+    //променя цвета си
+    public void changeColor() {
+        switch (this.color) {
+            case RED:
+                this.color = Color.GREEN;
+                break;
+            case GREEN:
+                this.color = Color.YELLOW;
+                break;
+            case YELLOW:
+                this.color = Color.RED;
+                break;
+        }
+    }
+
+    public String getColor() {
+        return this.color.name();
+    }
+}
