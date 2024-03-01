1d87dbd5ce8d1fea7f7ff5301a061d43ff75b9ed
Ali-Dzhan Sadak <118597212+Ali-Dzhan@users.noreply.github.com>
2024-03-01 10:43:22 +0200

    Add files via upload

---
+++ Java_OOP_February_2024/InterfacesAndAbstraction/P06Ferrari/Main.java
@@ -0,0 +1,13 @@
+package InterfacesAndAbstraction.P06Ferrari;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String driverName = new Scanner(System.in).nextLine();
+
+        Car ferrari = new Ferrari(driverName);
+
+        System.out.println(ferrari);
+    }
+}
