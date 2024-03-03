e7c502d4bbb5863d308207a7842eddd622c830fa
Ali-Dzhan Sadak <118597212+Ali-Dzhan@users.noreply.github.com>
2024-03-03 11:55:08 +0200

    Add files via upload

---
+++ Java_OOP_February_2024/InterfacesAndAbstractionExercise/P05Telephony/Browsable.java
@@ -0,0 +1,7 @@
+package InterfacesAndAbstractionExercise.P05Telephony;
+
+import java.util.List;
+
+public interface Browsable {
+    String browse();
+}
---
+++ Java_OOP_February_2024/InterfacesAndAbstractionExercise/P05Telephony/Callable.java
@@ -0,0 +1,8 @@
+package InterfacesAndAbstractionExercise.P05Telephony;
+
+import java.util.List;
+
+public interface Callable {
+
+    String call();
+}
