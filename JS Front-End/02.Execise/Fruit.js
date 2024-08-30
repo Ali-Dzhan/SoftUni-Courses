a9f93030898ebbb885cae9c0a389530ab35337b8
Ali-Dzhan Sadak <118597212+Ali-Dzhan@users.noreply.github.com>
2024-08-30 21:54:33 +0300

    Add files via upload

---
+++ JS Front-End/02.Execise/Fruit.js
@@ -0,0 +1,7 @@
+function solve(fruit, grams, price) {
+    const weight = grams / 1000;
+    const sum = weight * price;
+    console.log(`I need $${sum.toFixed(2)} to buy ${weight.toFixed(2)} kilograms ${fruit}.`);
+}
+
+solve("apple", 500, 3.50);
